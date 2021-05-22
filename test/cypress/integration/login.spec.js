2f9d989a5f1fd1d87300551ebd978f754f34fab3
Juan Francisco <contrerasjf.0@hotmail.com>
2021-05-22 16:05:34 -0500

    test(login): Implement Assertions to validate erros
    Resolve: C-09

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -14,13 +14,24 @@ describe('Login tests', () => {
     cy.get('#password2').type('qwerty123')
     cy.contains('.button', 'Registrarse').click()
     cy.wait(3000)
+    cy.get('.error-msg').should('not.exist')
   })
 
-  it('should sign in an user', () => {
+  it('Must fail the sign in with a non-registered-user', () => {
+    cy.visit('/login')
+    cy.get('#email1').type('non-siggned@user.io')
+    cy.get('#password1').type('qwerty123')
+    cy.contains('.button', 'Ingresar').click()
+    cy.wait(3000)
+    cy.get('.error-msg').should('be.visible')
+  })
+
+  it('must login a user', () => {
     cy.visit('/login')
     cy.get('#email1').type('test@test.io')
     cy.get('#password1').type('qwerty123')
     cy.contains('.button', 'Ingresar').click()
     cy.wait(3000)
+    cy.contains('a', 'Dashboard').should('be.visible')
   })
 })
