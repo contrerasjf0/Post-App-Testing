051cbc8554607d8c7a1a86e0e9f4532d5c0ad38f
Juan Francisco <contrerasjf.0@hotmail.com>
2021-05-22 15:58:41 -0500

    test(Login): Implement test set for login view
    * Add test to sign up user
    * Add test to sign in an user
    
    Resolve: C-08

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -1,7 +1,26 @@
 'use strict'
 
-describe('Login Test', () => {
-  it('Show the login view', ()  => {
+describe('Login tests', () => {
+  it('Should yield the login page', () => {
     cy.visit('/login')
+  })
+
+  it('should sign up a user', () => {
+    cy.visit('/login')
+    cy.contains('Crear una cuenta').click()
+    cy.get('#name').type('User tester')
+    cy.get('#title').type('Company tester')
+    cy.get('#email2').type('test@test.io')
+    cy.get('#password2').type('qwerty123')
+    cy.contains('.button', 'Registrarse').click()
+    cy.wait(3000)
+  })
+
+  it('should sign in an user', () => {
+    cy.visit('/login')
+    cy.get('#email1').type('test@test.io')
+    cy.get('#password1').type('qwerty123')
+    cy.contains('.button', 'Ingresar').click()
+    cy.wait(3000)
   })
 })
