23cff9372e781b82279d115c8b49a0d28d153f5d
Juan Francisco <contrerasjf.0@hotmail.com>
2021-05-22 16:24:19 -0500

    test(Login): Execute script to clear DB and implement Skipt hook
    * Create new script to run clean-test-db script
    * Execute script in before hook
    
    Resolve: C-12

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -1,6 +1,11 @@
 'use strict'
 
 describe('Login tests', () => {
+  
+  before(() => {
+    cy.exec('npm run test:clean')
+  })
+
   beforeEach(() => {
     cy.fixture('user.json').as('userData')
     cy.visit('/login')
@@ -8,20 +13,20 @@ describe('Login tests', () => {
   })
 
   it('should sign up a user', () => {
-    cy.get('@userData').then((userData)=>{
+    cy.get('@userData').then((userData) => {
       cy.contains('Crear una cuenta').click()
-      cy.get('#name').type('User tester')
-      cy.get('#title').type('Company tester')
-      cy.get('#email2').type('test@test.io')
-      cy.get('#password2').type('qwerty123')
+      cy.get('#name').type(userData.name)
+      cy.get('#title').type(userData.company)
+      cy.get('#email2').type(userData.email)
+      cy.get('#password2').type(userData.password)
       cy.contains('.button', 'Registrarse').click()
       cy.wait(3000)
       cy.get('.error-msg').should('not.exist')
     })
   })
 
-  it('Must fail the sign in with a non-registered-user', () => {
-    cy.get('@userData').then((userData)=>{
+  it.skipt('Must fail the sign in with a non-registered-user', () => {
+    cy.get('@userData').then((userData) => {
       cy.get('#email1').type('non-siggned@user.io')
       cy.get('#password1').type('qwerty123')
       cy.contains('.button', 'Ingresar').click()
@@ -31,11 +36,13 @@ describe('Login tests', () => {
   })
 
   it('must login a user', () => {
-    cy.get('#email1').type('test@test.io')
-    cy.get('#password1').type('qwerty123')
-    cy.contains('.button', 'Ingresar').click()
-    cy.wait(3000)
-    cy.contains('a', 'Dashboard').should('be.visible')
+    cy.get('@userData').then((userData) => {
+      cy.get('#email1').type(userData.email)
+      cy.get('#password1').type(userData.password)
+      cy.contains('.button', 'Ingresar').click()
+      cy.wait(3000)
+      cy.contains('a', 'Dashboard').should('be.visible')
+    })
   })
 
   after(() => {
