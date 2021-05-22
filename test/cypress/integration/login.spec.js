f1ed255dddccef825510c32bc1623bdf89a5934c
Juan Francisco <contrerasjf.0@hotmail.com>
2021-05-22 16:09:09 -0500

    test(login): Implement beforeEach and after hooks
    * Define the global variables on the package.jon
    * Implement beforeEach and after hooks
    
    Resolve: C-10

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -1,12 +1,13 @@
 'use strict'
 
 describe('Login tests', () => {
-  it('Should yield the login page', () => {
+
+  beforeEach(() => {
     cy.visit('/login')
+    cy.contains('h1', 'Bienvenido').should('be.visible')
   })
 
   it('should sign up a user', () => {
-    cy.visit('/login')
     cy.contains('Crear una cuenta').click()
     cy.get('#name').type('User tester')
     cy.get('#title').type('Company tester')
@@ -18,7 +19,6 @@ describe('Login tests', () => {
   })
 
   it('Must fail the sign in with a non-registered-user', () => {
-    cy.visit('/login')
     cy.get('#email1').type('non-siggned@user.io')
     cy.get('#password1').type('qwerty123')
     cy.contains('.button', 'Ingresar').click()
@@ -27,11 +27,15 @@ describe('Login tests', () => {
   })
 
   it('must login a user', () => {
-    cy.visit('/login')
     cy.get('#email1').type('test@test.io')
     cy.get('#password1').type('qwerty123')
     cy.contains('.button', 'Ingresar').click()
     cy.wait(3000)
     cy.contains('a', 'Dashboard').should('be.visible')
   })
+
+  after(() => {
+    cy.log('All tests done')
+  })
+
 })
