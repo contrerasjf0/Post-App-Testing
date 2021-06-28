06cf6d499609e0c6b1505562048ef3512f44e54c
Juan Francisco <contrerasjf.0@hotmail.com>
2021-06-28 18:41:51 -0500

    test(login): Create custom commands
    * Create custom commands for CreateUser and LoginUser test
    
    Resolve: C-13

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -14,33 +14,20 @@ describe('Login tests', () => {
 
   it('should sign up a user', () => {
     cy.get('@userData').then((userData) => {
-      cy.contains('Crear una cuenta').click()
-      cy.get('#name').type(userData.name)
-      cy.get('#title').type(userData.company)
-      cy.get('#email2').type(userData.email)
-      cy.get('#password2').type(userData.password)
-      cy.contains('.button', 'Registrarse').click()
-      cy.wait(3000)
-      cy.get('.error-msg').should('not.exist')
+      cy.CreateUser(userData)
     })
   })
 
   it.skipt('Must fail the sign in with a non-registered-user', () => {
     cy.get('@userData').then((userData) => {
-      cy.get('#email1').type('non-siggned@user.io')
-      cy.get('#password1').type('qwerty123')
-      cy.contains('.button', 'Ingresar').click()
-      cy.wait(3000)
+      cy.loginUser('non-siggned@user.io', 'qwerty123')
       cy.get('.error-msg').should('be.visible')
     })
   })
 
   it('must login a user', () => {
     cy.get('@userData').then((userData) => {
-      cy.get('#email1').type(userData.email)
-      cy.get('#password1').type(userData.password)
-      cy.contains('.button', 'Ingresar').click()
-      cy.wait(3000)
+      cy.loginUser(userData.email, userData.password)
       cy.contains('a', 'Dashboard').should('be.visible')
     })
   })
--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -23,3 +23,22 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('CreateUser', (userData) => {
+  cy.contains('Crear una cuenta').click()
+  cy.get('#name').type(userData)
+  cy.get('#title').type(userData)
+  cy.get('#email2').type(userData)
+  cy.get('#password2').type(userData)
+  cy.contains('.button', 'Registrarse').click()
+  cy.wait(3000)
+  cy.get('.error-msg').should('not.exist')
+
+})
+
+Cypress.Commands.add('LoginUser', (userName, password) => {
+  cy.get('#email1').type(userName)
+  cy.get('#password1').type(password)
+  cy.contains('.button', 'Ingresar').click()
+  cy.wait(3000)
+})
